db614be84ce6e6e565a29e4b1c536fd7f315751d
bahattin58 <bahattin_yilmaz@yahoo.com.tr>
2020-01-23 02:33:49 +0300

    Change customer veriables

--- src/main/java/com/crm/repository/ProvinceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.crm.repository;
-
-import com.crm.domain.Province;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProvinceRepository extends JpaRepository<Province,Long> {
-}
---
+++ src/main/java/com/crm/services/ISalesChannelService.java
@@ -0,0 +1,4 @@
+package com.crm.services;
+
+public class ISalesChannelService {
+}
